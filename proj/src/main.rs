06d824b20579414a79e865af6b55bd5a6dc8bfcd
dariusm01007 <dariusmensah01@outlook.com>
2024-06-02 16:00:31 -0700

    Update main.rs
    First attempt at using the server with a simple string as the address

--- proj/src/main.rs
+++ proj/src/main.rs
@@ -1,3 +1,14 @@
+// module name (files are modules in Rust) :: Struct name
+// Allows for shortening the names
+use server::Server;
+use http::Request;
+use http::Method;
+
+// This imports the modules
+mod server;
+mod http;
+
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
 }
